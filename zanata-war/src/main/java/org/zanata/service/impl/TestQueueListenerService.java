59c397d99ca0f21416299b4718e212bb5d7acb4e
Carlos A. Munoz <camunoz@redhat.com>
2014-04-30 10:22:35 +1000

    Add an initial sample use of JMS queues and listeners.
    JBoss needs to be configured to use queues. Please see standalone-full.xml for a valid configuration example.

---
+++ zanata-war/src/main/java/org/zanata/service/impl/TestQueueListenerService.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2014, Red Hat, Inc. and individual contributors as indicated by the
+ * @author tags. See the copyright.txt file in the distribution for a full
+ * listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
+ */
+package org.zanata.service.impl;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.Queue;
+import javax.jms.QueueReceiver;
+import javax.jms.QueueSession;
+import javax.jms.Session;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.jboss.seam.Component;
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Create;
+import org.jboss.seam.annotations.Destroy;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Observer;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.annotations.Startup;
+import org.jboss.seam.contexts.Lifecycle;
+import org.jboss.seam.jms.QueueConnection;
+import org.zanata.dao.ProjectDAO;
+
+/**
+ * @author Carlos Munoz <a href="mailto:camunoz@redhat.com">camunoz@redhat.com</a>
+ */
+@Name("testQueueListenerService")
+@Scope(ScopeType.APPLICATION)
+@Startup
+public class TestQueueListenerService implements MessageListener {
+
+    private QueueSession queueSession;
+
+    private QueueReceiver queueReceiver;
+
+    @Create
+    public void init() {
+        try {
+            Context ctx = new InitialContext();
+            queueSession = QueueConnection.instance().createQueueSession(false,
+                    Session.DUPS_OK_ACKNOWLEDGE);
+            Queue queue = (Queue)ctx.lookup("queue/test");
+            queueReceiver = queueSession.createReceiver(queue);
+            queueReceiver.setMessageListener(this);
+        }
+        catch (NamingException e) {
+            throw new RuntimeException("myListener initialization failed", e);
+        }
+        catch (JMSException e) {
+            throw new RuntimeException("myListener initialization failed", e);
+        }
+    }
+
+    @Override
+    public void onMessage(Message message) {
+        // Make sure Seam is live
+        Lifecycle.beginCall();
+
+        try {
+            Component.getInstance(ProjectDAO.class);
+            System.out.println("Got a message: " + message.toString());
+        }
+        finally {
+            Lifecycle.endCall();
+        }
+    }
+
+    @Destroy
+    public void destroy() {
+        if( queueReceiver != null ) {
+            try {
+                queueReceiver.close();
+            }
+            catch (JMSException e) {
+                // Proceed
+            }
+        }
+        if( queueSession != null ) {
+            try {
+                queueSession.close();
+            }
+            catch (JMSException e) {
+                // Proceed
+            }
+        }
+    }
+}
