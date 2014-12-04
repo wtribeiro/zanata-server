fd9bf144665c2ad28704abc89c534d57f96a7716
Sean Flanigan <sflaniga@redhat.com>
2014-12-04 15:51:17 +1000

    Check wildfly module downloads before use

--- functional-test/src/etc/wildflyModules.groovy
+++ functional-test/src/etc/wildflyModules.groovy
@@ -1,8 +1,19 @@
 
-def download(String address, File destFile) {
+def downloadZip(String address, File destFile) {
     def file = new FileOutputStream(destFile)
+    HttpURLConnection conn = new URL(address).openConnection()
+    if (conn.responseCode >= 400) {
+        throw new Exception("bad response code: " + conn.responseCode)
+    }
+    InputStream is = conn.inputStream
+    int p = is.read()
+    if (p != 'P') throw new Exception("response should start with PK: " +p)
+    int k = is.read()
+    if (k != 'K') throw new Exception("response should start with PK: " +k)
     def out = new BufferedOutputStream(file)
-    out << new URL(address).openStream()
+    out.write(p)
+    out.write(k)
+    out << is
     out.close()
 }
 
@@ -12,8 +23,8 @@ def hibernateUrl = "http://sourceforge.net/projects/zanata/files/wildfly/wildfly
 File mojarraZip = new File(project.build.directory, "mojarra.zip")
 File hibernateZip = new File(project.build.directory, "hibernate.zip")
 
-download(mojarraUrl, mojarraZip)
-download(hibernateUrl, hibernateZip)
+downloadZip(mojarraUrl, mojarraZip)
+downloadZip(hibernateUrl, hibernateZip)
 
 
 def ant = new AntBuilder()
